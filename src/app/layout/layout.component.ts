55749eda0f673cb974203a7128df9254389f5530
Jesse Sandvik <sandvikjesse@gmail.com>
2025-08-03 03:51:21 -0400

    icons, footer, slogan

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -2,11 +2,12 @@ import { Component } from "@angular/core";
 import { HeaderComponent } from "../header/header.component";
 import { RouterOutlet } from "@angular/router";
 import { FooterComponent } from "../footer/footer.component";
+import { CommonModule } from "@angular/common";
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.css'],
-  imports: [HeaderComponent, RouterOutlet, FooterComponent]
+  imports: [CommonModule, HeaderComponent, RouterOutlet, FooterComponent]
 })
 export class LayoutComponent {}
